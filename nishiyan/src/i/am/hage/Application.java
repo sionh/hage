3e08b43219995ee49efc8fee9d9162b971c17078
nag <nagppoi@gmail.com>
2016-07-16 01:09:32 +0900

    最新アプリ i am hage のソースを登録

---
+++ nishiyan/src/i/am/hage/Application.java
@@ -0,0 +1,13 @@
+package i.am.hage;
+
+public class Application {
+
+	public void run() {
+		System.out.println("i am hage");
+	}
+
+	public static void main(String[] args) {
+		Application application = new Application();
+		application.run();
+	}
+}
