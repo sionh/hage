d6e8314ef37a07ef64af33b0231bf532a62f6c5a
sion <shun_h@hotmail.com>
2016-07-30 17:37:34 +0900

    いいにしやんさんを作りました。

---
+++ nishiyan/src/i/am/hage/HairNegaNega.java
@@ -0,0 +1,8 @@
+package i.am.hage;
+
+public class HairNegaNega extends Hair {
+
+	public void fusa() {
+		System.out.println("fusa ...");
+	}
+}
---
+++ nishiyan/src/i/am/hage/NishiyanAge.java
@@ -0,0 +1,15 @@
+package i.am.hage;
+
+public class NishiyanAge extends Nishiyan {
+
+	public void setHage() {
+		hairs.clear();
+		System.out.println("osushi ogorune...!!");
+	}
+
+	public void setFusafusa() {
+		for (int i = 0; i < 100; i++) {
+			hairs.add(new HairAgeAge());
+		}
+	}
+}
